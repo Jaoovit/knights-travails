5e38be4f983d3121265301f2cd42ce9df6c9da22
João Oliveira <jaoovit@gmail.com>
2024-05-13 15:25:13 +0100

    create the board

---
+++ knights-travails.js
@@ -0,0 +1,11 @@
+function createBoard() {
+  let board = new Array();
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      board.push([i, j]);
+    }
+  }
+  return board;
+}
+
+console.log(createBoard());
