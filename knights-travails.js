4fe90f08d253d7d54ef84dddd8b1e820581514e1
João Oliveira <jaoovit@gmail.com>
2024-05-13 15:33:36 +0100

    create a function to find index and another to build info aboult the array

--- knights-travails.js
+++ knights-travails.js
@@ -8,4 +8,24 @@ function createBoard() {
   return board;
 }
 
-console.log(createBoard());
+function findIndex(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === target[0] && arr[i][1] === target[1]) {
+      return i;
+    }
+  }
+}
+
+function buildInfoArr(boardArr, startIndex) {
+  let newArr = new Array();
+  for (let i = 0; i < boardArr.length; i++) {
+    newArr[i] = {
+      distance: null,
+      prodecessor: null,
+    };
+  }
+  newArr[startIndex].distance = 0;
+  return newArr;
+}
+
+console.log(createBoard()[0]);
